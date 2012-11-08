f384b88b4dd5aa71ac0831ecc37d399189580607
Piotr Solnica <piotr.solnica@gmail.com>
2012-11-08 09:44:27 +0100

    Clean up ArelRelation#join a bit

--- lib/data_mapper/relation_registry/relation_node/arel_relation.rb
+++ lib/data_mapper/relation_registry/relation_node/arel_relation.rb
@@ -7,6 +7,7 @@ module DataMapper
       class ArelRelation < self
         include Enumerable
 
+        # @api public
         def each(&block)
           return to_enum unless block_given?
           relation.each do |row|
@@ -15,16 +16,49 @@ module DataMapper
           self
         end
 
+        # @api private
         def join(other, relationship)
-          left  = relation.source
-          right = other.relation.source
+          source     = relation.source
+          target     = other.relation.source
+          source_key = source[relationship.source_key]
+          target_key = target[relationship.target_key]
+          join       = source.join(target).on(source_key.eq(target_key)).order(source_key)
 
-          source_key = relationship.source_key
-          target_key = relationship.target_key
+          self.class.new(name, relation.new(join, join_header(other)))
+        end
 
-          join = left.join(right).on(left[source_key].eq(right[target_key])).order(left[:id])
+        # @api private
+        def base?
+          # TODO: push it down to ArelEngine::Gateway
+          relation.relation.kind_of?(Arel::Table)
+        end
 
-          # TODO: come up with a more abstract way of representing joined header (in AliasSet or AttributeSet?)
+        # @api private
+        def rename(new_aliases)
+          raise NotImplementedError
+        end
+
+        # @api private
+        def header
+          raise NotImplementedError
+        end
+
+        # @api private
+        def restrict(*args, &block)
+          raise NotImplementedError
+        end
+
+        # @api private
+        def sort_by(&block)
+          raise NotImplementedError
+        end
+
+        private
+
+        # TODO: come up with a more abstract way of representing joined header
+        #
+        # @api private
+        def join_header(other)
           left_header = aliases.attributes.map { |attribute|
             "#{name}.#{attribute.field} AS #{aliases[attribute.name]}"
           }
@@ -33,28 +67,7 @@ module DataMapper
             "#{other.name}.#{attribute.field} AS #{other.aliases[attribute.name]}"
           }
 
-          self.class.new(name, relation.new(join, left_header.concat(right_header)))
-        end
-
-        def base?
-          # TODO: push it down to ArelEngine::Gateway
-          relation.relation.kind_of?(Arel::Table)
-        end
-
-        def rename(new_aliases)
-          raise NotImplementedError
-        end
-
-        def header
-          raise NotImplementedError
-        end
-
-        def restrict(*args, &block)
-          raise NotImplementedError
-        end
-
-        def sort_by(&block)
-          raise NotImplementedError
+          left_header.concat(right_header)
         end
 
       end # class ArelRelation
