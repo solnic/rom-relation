e3d19053a4b0a887d4abe331a06414c38d789eaf
Indrek Juhkam <indrek@urgas.eu>
2013-03-11 19:35:51 +0200

    Add tests for Attribute#field

--- spec/unit/data_mapper/attribute/field_spec.rb
+++ spec/unit/data_mapper/attribute/field_spec.rb
@@ -1,7 +1,21 @@
 require 'spec_helper'
 
 describe Attribute, '#field' do
-  it 'needs unit specs' do
-    pending('Attribute#field unit specs are not implemented yet')
+  subject { attribute.field }
+
+  let(:attribute) { subclass.new(:title, options) }
+
+  context 'when "to" option given' do
+    let(:options) { {:to => :bookTitle} }
+
+    it { should be(:bookTitle) }
+  end
+
+  context 'when "to" option is not given' do
+    let(:options) { EMPTY_HASH }
+
+    it 'defaults to name' do
+      subject.should be(:title)
+    end
   end
 end
